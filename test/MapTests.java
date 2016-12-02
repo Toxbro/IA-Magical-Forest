b6aa341c857866a265a66dce1f4b77e046196276
Toxbro <darkred@hotmail.fr>
2016-12-02 14:15:52 -0500

    First environment version

---
+++ test/MapTests.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import ia.magical.forest.environment.Cell;
+import ia.magical.forest.environment.Map;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thomas
+ */
+public class MapTests {
+    
+    static Map map;
+    
+    public MapTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        map = new Map(3);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testConstructor(){
+        Cell monsterCell = map.getMonsterCell();
+        Cell crevasseCell = map.getCrevasseCell();
+        assertNotNull(monsterCell);
+        assertNotNull(crevasseCell);
+        assertNotEquals(monsterCell, crevasseCell);
+        System.out.println("Monster cell : "+monsterCell);
+        System.out.println("Crevasse cell : "+crevasseCell);
+    }
+    
+    @Test
+    public void testCloseCellsCenter(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(4));
+        assertEquals(4, returnCells.size());
+    }
+    
+    @Test
+    public void testCloseCellsTopLeft(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(0));
+        assertEquals(2, returnCells.size());
+    }
+    
+    @Test
+    public void testCloseCellsTopRight(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(2));
+        assertEquals(2, returnCells.size());
+    }
+    
+    @Test
+    public void testCloseCellsBottomLeft(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(6));
+        assertEquals(2, returnCells.size());
+    }
+    
+    @Test
+    public void testCloseCellsBottomRight(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(8));
+        assertEquals(2, returnCells.size());
+    }
+    
+    @Test
+    public void testCloseCellsTopCenter(){
+        ArrayList<Cell> returnCells = map.getCloseCells(map.getCells().get(1));
+        assertEquals(3, returnCells.size());
+    }
+    
+    @Test
+    public void testGetRandomCell(){
+        Cell cell = map.getRandomCell();
+        assertNotNull(cell);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
