629f1b7cb1d136a115468366871e353c7967e21c
Zibron <rollin.quentin@live.fr>
2016-12-07 17:52:51 -0500

    Modif prolog

--- Player.pl
+++ Player.pl
@@ -1,33 +1,68 @@
-position(pos)
-java_object(Controller, [], controller)
+java_object(Player, [], player)
+
+pos(0,0).
+crackFound(0).
+monsterFound(0).
+alreadyVisited([]).
 
 oppositeDirection(LEFT,RIGHT).
 oppositeDirection(RIGHT,LEFT).
 oppositeDirection(UP,DOWN).
 oppositeDirection(DOWN,UP).
 
+rule 1: windy(X) => assert(crack(UP)), assert(crack(DOWN)), assert(crack(LEFT)), assert(crack(RIGHT)), opposite(lastMove, Y), retract(crack(Y)).
+rule 1: smelly(X) => assert(smelly(UP)), assert(smelly(DOWN)), assert(smelly(LEFT)), assert(smelly(RIGHT)), opposite(lastMove, Y), retract(smelly(Y)).
+rule 3 : windy(X)	
+rule 4 : move(_) => retract(smelly(_)), retract(windy(_)).
 
 
-act :- 
-				controller <- isPortal returns portal,
+move(UP) :- pos(X,Y), append(pos(X,Y), alreadyVisited(L)), Y1 is Y, Y is Y1-1.
+move(DOWN) :- pos(X,Y), Y1 is Y, Y is Y1+1.
+move(LEFT) :- pos(X,Y), X1 is X, Y is X1-1.
+move(RIGHT) :- pos(X,Y), X1 is X, Y is X1+1.
+move(_) :- 
+
+
+
+/*act(X) :- 
+				Player <- isPortal returns portal,
 				portal == true,
 				finish.
 				
-act :- 
-				controller <- isWind returns wind
-				controller <- isSmell returns smell
+act(X) :- 
+				Player <- isWind returns wind,
+				Player <- noChoice returns boolean,
+				boolean == true,
+				
+
+act(X) :- 
+				Player <- isWind returns wind
+				Player <- noChoice returns boolean,
+				boolean == false,
+				
+
+act(X) :- 
+				Player <- isSmell returns smell
+
+
+act(X) :- 
+				Player <- isSmell returns smell
+
+act(X) :-
+				
+
 
 finish :-
-				controller <- finish.
+				Player <- finish.
 
 move(Direction) :- 
 									first(path,X),
 									!oppositeDirection(Direction, X),
-									controller <- canIMove(Direction) returns boolean,
+									Player <- canIMove(Direction) returns boolean,
 									boolean == true,
-									controller <- moveTo(Direction),
+									Player <- moveTo(Direction),
 									append(Direction, path, path).
 
-first([H|T],H).
+first([H|T],H).*/
 
 
